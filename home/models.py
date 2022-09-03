216d2abd436ecde34c48c254efc0172796933433
Vid Pesko <vid@pesko.si>
2022-09-03 20:23:57 +0200

    Edited Home and started working on NAVBAR

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Item(models.Model):
+    pass
+    # user = models.ForeignKey(User)
+    # name = models.CharField(max_length=50)
+    # release_date
+    # country
+    # value
+    # collection
+    # unit
+    # item_type
+    # description
--- home/views.py
+++ home/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from .models import Topic
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/Domov.html')
+    # TOPICS
+    topics = Topic.objects.all()
+
+    # NUMBERS
+    num_of_users = len(User.objects.all())
+    average_size = 12
+    biggest_collection = 30
+    all_collected = 100
+    statistics = {
+        'users':num_of_users,
+        'average_size': average_size,
+        'biggest_collection': biggest_collection,
+        'all_collected':all_collected,
+    }
+
+    context = {'topics':topics, 'stats':statistics}
+    return render(request, 'home/Domov.html', context)
